0e799b99b3b9dc1463fdd8b8aa3a2571aa74d961
AlexFlipnote <alexander.samuelsen.manso@gmail.com>
2024-02-21 15:55:31 +0100

    Cleanup backend

--- discord_http/backend.py
+++ discord_http/backend.py
@@ -13,6 +13,7 @@ from quart.logging import default_handler
 from quart.utils import MustReloadError, restart
 from typing import Optional, Any, Union, TYPE_CHECKING
 
+from . import utils
 from .commands import Command, Interaction, Listener, SubGroup
 from .enums import InteractionType
 from .errors import CheckFailed
@@ -64,7 +65,7 @@ class DiscordHTTP(Quart):
         This serves as the fundemental HTTP server for Discord Interactions
         We recommend to not touch this class, unless you know what you're doing
         """
-        self.uptime: datetime = datetime.now()
+        self.uptime: datetime = utils.utcnow()
 
         self.bot: "Client" = client
         self.loop = self.bot.loop
@@ -81,8 +82,11 @@ class DiscordHTTP(Quart):
         _quart_log.removeHandler(default_handler)
         _quart_log.setLevel(logging.CRITICAL)
 
-    async def validate_request(self) -> None:
-        """ Used to validate requests sent by Discord Webhooks """
+    async def _validate_request(self) -> None:
+        """
+        Used to validate requests sent by Discord Webhooks
+        This should NOT be modified, unless you know what you're doing
+        """
         if not self.bot.public_key:
             return abort(401, "invalid public key")
 
@@ -102,38 +106,14 @@ class DiscordHTTP(Quart):
         except Exception:
             abort(400, "invalid request body")
 
-    def error_messages(
-        self,
-        ctx: "Context",
-        e: Exception
-    ) -> Optional[MessageResponse]:
-        """
-        Used to return error messages to Discord
-
-        Parameters
-        ----------
-        ctx: `Context`
-            The context of the command
-        e: `Exception`
-            The exception that was raised
-
-        Returns
-        -------
-        `Optional[MessageResponse]`
-            The message response provided by the library error handler
-        """
-        if isinstance(e, CheckFailed):
-            return ctx.response.send_message(
-                content=str(e),
-                ephemeral=True
-            )
-
     def _dig_subcommand(
         self,
         cmd: Union[Command, SubGroup],
         data: dict
     ) -> tuple[Optional[Command], list[dict]]:
-        """ Used to dig through subcommands to execute correct command/autocomplete """
+        """
+        Used to dig through subcommands to execute correct command/autocomplete
+        """
         data_options: list[dict] = data["data"].get("options", [])
 
         while isinstance(cmd, SubGroup):
@@ -158,12 +138,174 @@ class DiscordHTTP(Quart):
 
         return cmd, data_options
 
-    async def _index_interaction(self) -> Union[BaseResponse, QuartResponse, dict]:
+    def _handle_ack_ping(
+        self,
+        ctx: "Context",
+        data: dict
+    ) -> dict:
+        """ Used to handle ACK ping """
+        _ping = Ping(state=self.bot.state, data=data)
+
+        if self.bot.has_any_dispatch("ping"):
+            self.bot.dispatch("ping", _ping)
+        else:
+            _log.info(f"Discord Interactions ACK recieved ({_ping.id})")
+
+        return ctx.response.pong()
+
+    async def _handle_application_command(
+        self,
+        ctx: "Context",
+        data: dict
+    ) -> Union[QuartResponse, dict]:
+        """ Used to handle application commands """
+        _log.debug("Received slash command, processing...")
+
+        command_name = data["data"]["name"]
+        cmd = self.bot.commands.get(command_name)
+        if not cmd:
+            _log.warn(
+                f"Unhandeled command: {command_name} "
+                "(not found in local command list)"
+            )
+            return QuartResponse(
+                "command not found",
+                status=404
+            )
+
+        cmd, _ = self._dig_subcommand(cmd, data)
+
+        try:
+            payload = await cmd._make_context_and_run(
+                context=ctx
+            )
+
+            return QuartResponse(
+                payload.to_multipart(),
+                content_type=payload.content_type
+            )
+        except Exception as e:
+            if self.bot.has_any_dispatch("interaction_error"):
+                self.bot.dispatch("interaction_error", ctx, e)
+            else:
+                _log.error(
+                    f"Error while running command {cmd.name}",
+                    exc_info=e
+                )
+
+            _send_error = self.error_messages(ctx, e)
+            if _send_error and isinstance(_send_error, BaseResponse):
+                return _send_error.to_dict()
+
+            return abort(500)
+
+    async def _handle_interaction(
+        self,
+        ctx: "Context",
+        data: dict
+    ) -> Union[QuartResponse, dict]:
+        """ Used to handle interactions """
+        _log.debug("Received interaction, processing...")
+        _custom_id = data["data"]["custom_id"]
+
+        try:
+            if ctx.message:
+                local_view = self.bot._view_storage.get(
+                    ctx.message.id, None
+                )
+                if local_view:
+                    payload = await local_view.callback(ctx)
+                    return QuartResponse(
+                        payload.to_multipart(),
+                        content_type=payload.content_type
+                    )
+
+            intreact = self.bot.find_interaction(_custom_id)
+            if not intreact:
+                _log.debug(
+                    "Unhandled interaction recieved "
+                    f"(custom_id: {_custom_id})"
+                )
+                return QuartResponse(
+                    "interaction not found",
+                    status=404
+                )
+
+            payload = await intreact.run(ctx)
+            return QuartResponse(
+                payload.to_multipart(),
+                content_type=payload.content_type
+            )
+
+        except Exception as e:
+            if self.bot.has_any_dispatch("interaction_error"):
+                self.bot.dispatch("interaction_error", ctx, e)
+            else:
+                _log.error(
+                    f"Error while running interaction {_custom_id}",
+                    exc_info=e
+                )
+
+            return abort(500)
+
+    async def _handle_autocomplete(
+        self,
+        ctx: "Context",
+        data: dict
+    ) -> Union[QuartResponse, dict]:
+        """ Used to handle autocomplete interactions """
+        _log.debug("Received autocomplete interaction, processing...")
+
+        command_name = data.get("data", {}).get("name", None)
+        cmd = self.bot.commands.get(command_name)
+
+        try:
+            if not cmd:
+                _log.warn(f"Unhandled autocomplete recieved (name: {command_name})")
+                return QuartResponse(
+                    "command not found",
+                    status=404
+                )
+
+            cmd, data_options = self._dig_subcommand(cmd, data)
+
+            find_focused = next((
+                x for x in data_options
+                if x.get("focused", False)
+            ), None)
+
+            if not find_focused:
+                _log.warn(
+                    "Failed to find focused option in autocomplete "
+                    f"(cmd name: {command_name})"
+                )
+                return QuartResponse(
+                    "focused option not found",
+                    status=400
+                )
+
+            return await cmd.run_autocomplete(
+                ctx, find_focused["name"], find_focused["value"]
+            )
+        except Exception as e:
+            if self.bot.has_any_dispatch("interaction_error"):
+                self.bot.dispatch("interaction_error", ctx, e)
+            else:
+                _log.error(
+                    f"Error while running autocomplete {cmd.name}",
+                    exc_info=e
+                )
+            return abort(500)
+
+    async def _index_interactions_endpoint(
+        self
+    ) -> Union[QuartResponse, dict]:
         """
         The main function to handle all HTTP requests sent by Discord
         Please do not touch this function, unless you know what you're doing
         """
-        await self.validate_request()
+        await self._validate_request()
+
         data = await request.json
 
         if self.debug_events:
@@ -174,146 +316,57 @@ class DiscordHTTP(Quart):
 
         match data_type:
             case InteractionType.ping:
-                _ping = Ping(state=self.bot.state, data=data)
-                if self.bot.has_any_dispatch("ping"):
-                    self.bot.dispatch("ping", _ping)
-                else:
-                    _log.info(f"Discord HTTP Ping | {_ping}")
-                return context.response.pong()
+                return self._handle_ack_ping(context, data)
 
             case InteractionType.application_command:
-                _log.debug("Received slash command, processing...")
-
-                command_name = data["data"]["name"]
-                cmd = self.bot.commands.get(command_name)
-                if not cmd:
-                    _log.warn(
-                        f"Unhandeled command: {command_name} "
-                        "(not found in local command list)"
-                    )
-                    return QuartResponse(
-                        "command not found",
-                        status=404
-                    )
-
-                cmd, data_options = self._dig_subcommand(cmd, data)
-
-                try:
-                    payload = await cmd._make_context_and_run(
-                        context=context
-                    )
-
-                    return QuartResponse(
-                        payload.to_multipart(),
-                        content_type=payload.content_type
-                    )
-                except Exception as e:
-                    if self.bot.has_any_dispatch("interaction_error"):
-                        self.bot.dispatch("interaction_error", context, e)
-                    else:
-                        _log.error(
-                            f"Error while running command {cmd.name}",
-                            exc_info=e
-                        )
-
-                    _send_error = self.error_messages(context, e)
-                    if _send_error and isinstance(_send_error, BaseResponse):
-                        return _send_error.to_dict()
-                    return abort(500)
+                return await self._handle_application_command(
+                    context, data
+                )
 
             case x if x in (
                 InteractionType.message_component,
                 InteractionType.modal_submit
             ):
-                _log.debug("Received interaction, processing...")
-                _custom_id = data["data"]["custom_id"]
-
-                try:
-                    if context.message:
-                        local_view = self.bot._view_storage.get(
-                            context.message.id,
-                            None
-                        )
-                        if local_view:
-                            payload = await local_view.callback(context)
-                            return QuartResponse(
-                                payload.to_multipart(),
-                                content_type=payload.content_type
-                            )
-
-                    intreact = self.bot.find_interaction(_custom_id)
-                    if not intreact:
-                        _log.debug(
-                            "Unhandled interaction recieved "
-                            f"(custom_id: {_custom_id})"
-                        )
-                        return QuartResponse(
-                            "interaction not found",
-                            status=404
-                        )
-
-                    payload = await intreact.run(context)
-                    return QuartResponse(
-                        payload.to_multipart(),
-                        content_type=payload.content_type
-                    )
-                except Exception as e:
-                    if self.bot.has_any_dispatch("interaction_error"):
-                        self.bot.dispatch("interaction_error", context, e)
-                    else:
-                        _log.error(
-                            f"Error while running interaction {_custom_id}",
-                            exc_info=e
-                        )
-                    return abort(500)
+                return await self._handle_interaction(
+                    context, data
+                )
 
             case InteractionType.application_command_autocomplete:
-                _log.debug("Received autocomplete interaction, processing...")
-
-                command_name = data.get("data", {}).get("name", None)
-                cmd = self.bot.commands.get(command_name)
-
-                try:
-                    if not cmd:
-                        _log.warn(f"Unhandled autocomplete recieved (name: {command_name})")
-                        return QuartResponse(
-                            "command not found",
-                            status=404
-                        )
-
-                    cmd, data_options = self._dig_subcommand(cmd, data)
-
-                    find_focused = next((
-                        x for x in data_options
-                        if x.get("focused", False)
-                    ), None)
-
-                    if not find_focused:
-                        _log.warn(
-                            "Failed to find focused option in autocomplete "
-                            f"(cmd name: {command_name})"
-                        )
-                        return QuartResponse(
-                            "focused option not found",
-                            status=400
-                        )
-
-                    return await cmd.run_autocomplete(
-                        context, find_focused["name"], find_focused["value"]
-                    )
-                except Exception as e:
-                    if self.bot.has_any_dispatch("interaction_error"):
-                        self.bot.dispatch("interaction_error", context, e)
-                    else:
-                        _log.error(
-                            f"Error while running autocomplete {cmd.name}",
-                            exc_info=e
-                        )
-                    return abort(500)
+                return await self._handle_autocomplete(
+                    context, data
+                )
 
             case _:  # Unknown
                 _log.debug(f"Unhandled interaction recieved (type: {data_type})")
                 return abort(400, "invalid request body")
+
+    def error_messages(
+        self,
+        ctx: "Context",
+        e: Exception
+    ) -> Optional[MessageResponse]:
+        """
+        Used to return error messages to Discord.
+        By default, it will only cover CheckFailed errors.
+        You can overwrite this function to return your own error messages.
+
+        Parameters
+        ----------
+        ctx: `Context`
+            The context of the command
+        e: `Exception`
+            The exception that was raised
+
+        Returns
+        -------
+        `Optional[MessageResponse]`
+            The message response provided by the library error handler
+        """
+        if isinstance(e, CheckFailed):
+            return ctx.response.send_message(
+                content=str(e),
+                ephemeral=True
+            )
 
     async def index_ping(self) -> Union[tuple[dict, int], dict]:
         """
@@ -333,7 +386,7 @@ class DiscordHTTP(Quart):
             },
             "last_reboot": {
                 "datetime": str(self.uptime.astimezone().isoformat()),
-                "timedelta": str(datetime.now() - self.uptime),
+                "timedelta": str(utils.utcnow() - self.uptime),
                 "unix": int(self.uptime.timestamp()),
             }
         }
@@ -346,7 +399,8 @@ class DiscordHTTP(Quart):
     ) -> None:
         if not self.bot.disable_default_get_path:
             self.add_url_rule(
-                "/", "ping",
+                "/",
+                "ping",
                 self.index_ping,
                 methods=["GET"]
             )
@@ -354,7 +408,7 @@ class DiscordHTTP(Quart):
         self.add_url_rule(
             "/",
             "index",
-            self._index_interaction,
+            self._index_interactions_endpoint,
             methods=["POST"]
         )
 
@@ -384,10 +438,16 @@ class DiscordHTTP(Quart):
         for signal_name in {"SIGINT", "SIGTERM", "SIGBREAK"}:
             if hasattr(signal, signal_name):
                 try:
-                    loop.add_signal_handler(getattr(signal, signal_name), _signal_handler)
+                    loop.add_signal_handler(
+                        getattr(signal, signal_name),
+                        _signal_handler
+                    )
                 except NotImplementedError:
                     # Add signal handler may not be implemented on Windows
-                    signal.signal(getattr(signal, signal_name), _signal_handler)
+                    signal.signal(
+                        getattr(signal, signal_name),
+                        _signal_handler
+                    )
 
         server_name = self.config.get("SERVER_NAME")
         sn_host = None
@@ -438,10 +498,14 @@ class DiscordHTTP(Quart):
         config.access_log_format = "%(h)s %(r)s %(s)s %(b)s %(D)s"
         config.accesslog = None
         config.bind = [f"{host}:{port}"]
-        config.debug = False
         config.ca_certs = None
         config.certfile = None
+        config.debug = False
         config.errorlog = None
         config.keyfile = None
 
-        return serve(self, config, shutdown_trigger=shutdown_trigger)
+        return serve(
+            self,
+            config,
+            shutdown_trigger=shutdown_trigger
+        )
